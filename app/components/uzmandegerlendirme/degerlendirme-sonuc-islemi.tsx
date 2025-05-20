00161f86f5b79123a0a02aae4e31ce797c151241
Murat Hayaloğlu <murat.hayaloglu@csgb.gov.tr>
2025-05-20 16:54:47 +0300

    degerlendirme ekranı ve degerlendirme  sonucu kısmı

--- app/components/uzmandegerlendirme/degerlendirme-sonuc-islemi.tsx
+++ app/components/uzmandegerlendirme/degerlendirme-sonuc-islemi.tsx
@@ -1,91 +1,155 @@
 "use client";
 
 import { useState } from "react";
-import { MoreHorizontal } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
-import { Checkbox } from "@/components/ui/checkbox";
+
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select";
+  CheckCircle,
+  ExternalLink,
+  PackagePlus,
+  RefreshCcw,
+  X,
+} from "lucide-react";
+import { RadioGroup } from "@/components/ui/radio-group";
+import { RadioGroupItem } from "@radix-ui/react-radio-group";
+import { Label } from "@/components/ui/label";
+import { cn } from "@/lib/utils";
+import { Textarea } from "@/components/ui/textarea";
+
+type Option = {
+  id: string;
+  title: string;
+  cssValue?: string;
+  icon: React.ReactNode;
+};
 
 export function DegerlendirmeSonucIslemi() {
-  const [confirmed, setConfirmed] = useState(false);
+  const [yapilanIslem, setYapilanIslem] = useState<string>("");
+
+  const [islemNotu, setIslemNotu] = useState<string>("");
+
+  const [selected, setSelected] = useState<string>("");
+
+  const options: Option[] = [
+    {
+      id: "option1",
+      title: "Onay",
+      cssValue: "bg-green-400 text-white",
+      icon: <PackagePlus className="h-6 w-6 text-white" />,
+    },
+    {
+      id: "option2",
+      title: "Ret",
+      cssValue: "bg-red-400 text-white",
+      icon: <X className="h-6 w-6 text-white" />,
+    },
+    {
+      id: "option3",
+      title: "Güncelleme",
+      cssValue: "bg-orange-400 text-white",
+      icon: <RefreshCcw className="h-6 w-6 text-white" />,
+    },
+    {
+      id: "option4",
+      cssValue: "bg-gray-400 text-white",
+      title: "Dış Kurum Görüş",
+      icon: <ExternalLink className="h-6 w-6 text-white" />,
+    },
+  ];
 
   return (
     <Card className="border border-blue-600">
       <CardHeader className="flex flex-row items-center justify-between">
-        <CardTitle className="text-xl font-bold flex items-center justify-center gap-2 cursor-pointer hover:text-blue-600 h-[35]">
-          Delete Account
+        <CardTitle className="text-xl font-bold flex items-center justify-center gap-2 cursor-pointer hover:text-blue-600 h-[15px]">
+          Değerlendirme Sonucu
         </CardTitle>
-        <div className="flex items-center gap-2">
-          <Select defaultValue="permanent">
-            <SelectTrigger className="w-[140px]">
-              <SelectValue placeholder="Deletion type" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectItem value="permanent">Permanent</SelectItem>
-              <SelectItem value="temporary">Temporary (30 days)</SelectItem>
-              <SelectItem value="archive">Archive Only</SelectItem>
-            </SelectContent>
-          </Select>
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <MoreHorizontal className="h-4 w-4" />
-                <span className="sr-only">More options</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="end">
-              <DropdownMenuItem>Download data</DropdownMenuItem>
-              <DropdownMenuItem>Request data report</DropdownMenuItem>
-              <DropdownMenuItem>Contact support</DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
-        </div>
+        <div className="flex items-center gap-2"></div>
       </CardHeader>
-      <CardContent className="space-y-4">
-        <p className="text-sm text-muted-foreground">
-          We regret to see you leave. Confirm account deletion below. Your data
-          will be permanently removed. Thank you for being part of our
-          community. Please check our{" "}
-          <a href="#" className="text-primary underline">
-            safety guidelines
-          </a>{" "}
-          if you still wish continue.
-        </p>
+      <CardContent className="space-y-4 ">
+        <RadioGroup
+          value={selected}
+          onValueChange={setSelected}
+          className="grid grid-cols-1 md:grid-cols-4 gap-3 w-full  "
+        >
+          {options.map((option) => (
+            <div key={option.id} className={`relative  `}>
+              <RadioGroupItem
+                value={option.id}
+                id={option.id}
+                className="sr-only "
+              />
+              <Label
+                htmlFor={option.id}
+                className="cursor-pointer block h-full"
+              >
+                <Card
+                  onClick={() => setYapilanIslem(option.title)}
+                  className={cn(
+                    `h-full transition-all border-2 ${
+                      option.cssValue ? option.cssValue : ""
+                    }`,
+                    selected === option.id
+                      ? "border-primary shadow-md"
+                      : "border-muted hover:border-muted-foreground/50"
+                  )}
+                >
+                  <CardContent
+                    className={`flex flex-col  items-center justify-center w-full `}
+                  >
+                    <div
+                      className={cn(
+                        `absolute top-3 right-3 `,
+                        selected === option.id ? "visible" : "invisible"
+                      )}
+                    >
+                      <CheckCircle className="h-6 w-6 text-primary fill-primary" />
+                    </div>
+                    <div className="">{option.icon}</div>
+                    <div className="flex flex-col gap-1">
+                      <h3 className="font-medium text-base">{option.title}</h3>
+                    </div>
+                  </CardContent>
+                </Card>
+              </Label>
+            </div>
+          ))}
+        </RadioGroup>
+        {yapilanIslem && (
+          <>
+            <div className="flex flex-col items-start gap-4 ">
+              <label
+                htmlFor="confirm"
+                className="text-sm text-left font-medium pl-2 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+              >
+                {yapilanIslem} İşlemine dair açıklamanızı yazın.
+              </label>
+              <Textarea
+                onChange={(e) => setIslemNotu(e.target.value)}
+                placeholder="Notunuzu buraya yazın"
+              />
+            </div>
 
-        <div className="flex items-center space-x-2">
-          <Checkbox
-            id="confirm"
-            checked={confirmed}
-            onCheckedChange={(checked) => setConfirmed(checked as boolean)}
-          />
-          <label
-            htmlFor="confirm"
-            className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-          >
-            Confirm deleting account
-          </label>
-        </div>
-
-        <div className="flex justify-end space-x-2">
-          <Button variant="outline">Deactivate Instead</Button>
-          <Button variant="destructive" disabled={!confirmed}>
-            Delete Account
-          </Button>
-        </div>
+            <div className="flex justify-end space-x-2">
+              <Button className="cursor-pointer" variant="outline">
+                İşlemi Kaydet 
+              </Button>
+              <Button onClick={() => {}} variant="destructive" 
+              className={
+                `
+                ${selected==="option1"&&"bg-green-400 text-white"}
+                ${selected==="option12"&&"bg-red-400 text-white"}
+                ${selected==="option3"&&"bg-orange-400 text-white"}
+                ${selected==="option4"&&"bg-gray-400 text-white"}
+                `
+                }>
+                
+                {yapilanIslem} işlemini kaydet.
+              </Button>
+            </div>
+          </>
+        )}
       </CardContent>
     </Card>
   );
