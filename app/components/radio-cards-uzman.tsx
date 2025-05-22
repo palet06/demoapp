507ffdeb2fe6cc2ca692e8c0864560df303b4bbe
Murat Hayaloğlu <murat.hayaloglu@csgb.gov.tr>
2025-05-22 17:23:55 +0300

    ek

--- app/components/radio-cards-uzman.tsx
+++ app/components/radio-cards-uzman.tsx
@@ -3,7 +3,7 @@
 
 import type React from "react";
 
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { Card, CardContent } from "@/components/ui/card";
 import { Label } from "@/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@/components/ui/radio-group";
